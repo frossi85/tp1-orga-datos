ff886f3b2decccf0564969fa7d65223e9688548f
pablo.arlia@gmail.com <pablo.arlia@gmail.com@2519ec38-0079-1e11-3c6c-8a54d3565cf0>
2011-10-03 03:57:18 +0000

    El nuevo main, que funciona con la clase Menu

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <cstdlib>
+
+#include "Menu.h"
+
 using namespace std;
 
 void mostrar_ayuda();
@@ -10,87 +13,46 @@ string pedir_contrasena();
 bool confirmar_usuario(int,string, string);
 
 int main(int argc, char *argv[]){
-
+	Menu *menu = Menu::getMenu();
 	if (argc != 2){
-
 		// Numero incorrecto de parametros
-
 		cout << "Uso: voto_electronico -h\n";
 		return 0;
 	}
 
 	if ( !(strcmp(argv[1],"-h")) || !(strcmp(argv[1],"--help")) ) {
-
 		// La funcionalidad deseada es acceder a la ayuda del programa.
-		mostrar_ayuda();
-
+		menu->mostrar_ayuda();
 		return 0;
-
 	}
 
 	if ( !(strcmp(argv[1],"-a")) || !(strcmp(argv[1],"--admin")) ) {
 
+		cout << "Ingreso como administrador. \n(para salir ingrese \"salir\" en alguno de los campos) \n";
 		// La funcionalidad deseada es acceder como administrador
 		// Se pide y verifica usuario y contraseña
+		do {
+			menu->pedir_usuario();
+			menu->pedir_contrasena();
+		} while (!menu->acceder_admin());
 
-		string usuario = pedir_usuario();
-		string password = pedir_contrasena();
-		bool es_usuario = confirmar_usuario(1, usuario, password);  // el 1 indica que se intenta acceder como administrador
-
-		/* if (es_usuario){
-			ACA COMIENZA EL ACCESO COMO ADMINISTRADOR
-		 }*/
 		return 0;
 	}
 	if ( !(strcmp(argv[1],"-u")) || !(strcmp(argv[1],"--user")) ) {
 
+		cout << "Ingreso como usuario. \n(para salir ingrese \"salir\" en alguno de los campos) \n";
 		// La funcionalidad deseada es acceder como usuario
 		// Se pide y verifica usuario y contraseña
+		do {
+			menu->pedir_usuario();
+			menu->pedir_contrasena();
+		} while (!menu->acceder_usuario());
 
-		string usuario = pedir_usuario();
-		string password = pedir_contrasena();
-		bool es_usuario = confirmar_usuario(0, usuario, password); // el 0 indica que se intenta acceder como usuario
-
-		/* if (es_usuario){
-			ACA COMIENZA EL ACCESO COMO USUARIO
-		 }*/
 		return 0;
 	}
 
-	cout << "Uso: voto_electronico -h\n";	// Mal uso de las opciones
+	cout << "Uso: voto_electronico -h\n";   // Mal uso de las opciones
 
 	return 0;
 }
 
-
-void mostrar_ayuda(){
-	cout << "*************************************************************\n";
-	cout << "Voto Electronico - Ayuda\n\n" << "Opciones:\n";
-	cout << "-h, --help: Muestra esta ayuda\n";
-	cout << "-u, --user: Login al sistema en calidad de usuario\n";
-	cout << "-a, --admin: Login al sistema en calidad de administrador\n\n";
-	cout << "Consulte el manual para mas informacion\n";
-	cout << "*************************************************************\n";
-}
-
-string pedir_usuario(){
-	char user[20];
-	cout<<("Usuario: ");
-	scanf("%s",user); //en vez de scanf por q no usar cin?? tratemos de no mezclar codigo c con c++
-	return user;
-}
-
-string pedir_contrasena(){
-	char pass[20];
-	cout<<("Contraseña: ");
-	system("stty -echo");
-	scanf("%s",pass);
-	system("stty echo");
-	printf("\nEscribiste (esto es para probar, desp se borra del codigo): ");	// ESTO DESP SE BORRA, ES PARA PROBAR
-	printf("%s\n",pass);
-	return pass;
-}
-
-bool confirmar_usuario(int,string, string){
-	cout<<"ACA SE CONFIRMA EL ACCESO\n";
-}
