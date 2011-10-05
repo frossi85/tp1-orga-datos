7f62813951abd737e2dddd6da07c75ccdca1cbb9
luceromartinandres89630 <luceromartinandres89630@2519ec38-0079-1e11-3c6c-8a54d3565cf0>
2011-10-05 07:43:28 +0000

    Subo esto pero todavia no se puede usar. Estoy probandolo de a poco en otro proyecto.

---
+++ src/NodoHoja.cpp
@@ -0,0 +1,17 @@
+/*
+ * NodoHoja.cpp
+ *
+ *  Created on: 02/10/2011
+ *      Author: martin
+ */
+
+#include "NodoHoja.h"
+
+NodoHoja::NodoHoja() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NodoHoja::~NodoHoja() {
+	// TODO Auto-generated destructor stub
+}
