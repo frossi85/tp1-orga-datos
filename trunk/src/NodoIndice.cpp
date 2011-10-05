89cc0fbf55c1a84ae407bcbe68ac3f6a29af7a80
luceromartinandres89630 <luceromartinandres89630@2519ec38-0079-1e11-3c6c-8a54d3565cf0>
2011-10-05 07:44:15 +0000

    Subo esto pero todavia no se puede usar. Estoy probandolo de a poco en otro proyecto.

---
+++ trunk/src/NodoIndice.cpp
@@ -0,0 +1,18 @@
+/*
+ * NodoIndice.cpp
+ *
+ *  Created on: 02/10/2011
+ *      Author: martin
+ */
+
+#include "NodoIndice.h"
+
+NodoIndice::NodoIndice() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NodoIndice::~NodoIndice() {
+	// TODO Auto-generated destructor stub
+}
+
