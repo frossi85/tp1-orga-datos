051ea60b5ab5a2747db1982c8529d14d0b11aa45
leandro.v.059@gmail.com <leandro.v.059@gmail.com@2519ec38-0079-1e11-3c6c-8a54d3565cf0>
2011-11-18 00:12:45 +0000

    Subo la estructura del algoritmo de Kasiski. Lo hice como una clase estática porque me pareció que no tenía mucho sentido que se pudiera instanciar. El único método público es Romper, que recibe la ruta del criptograma, y la ruta de destino donde se va a guardar el archivo "desencriptado". Ya la incluí en el makefile y compila.
    También agrego una carpeta "Criptografía" para que vayamos metiendo ahi todo lo que tenga que ver con estos algoritmos.
    
    No creo que pueda rellenar la estructura con métodos de alguna biblioteca externa porque no encontré ninguna. Lo único que encontré fueron un par de archivos de código fuente que implementan el algoritmo y los estoy usndo para guiarme, pero son programas completos en C con el main y todo. Me parece que va a ser mejor que arranque de una a programar el algoritmo, si trato de adaptar eso para que ande en mi clase tendría que cambiar tantas cosas que es mas o menos lo mismo.

---
+++ trunk/src/Criptografía/Kasiski.h
@@ -0,0 +1,70 @@
+#ifndef KASISKI_H
+#define	KASISKI_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Kasiski {
+private:
+
+    //tipos
+    typedef struct _longitudPosible {
+        unsigned long longitud;
+        unsigned long ocurrencias;
+    } longitudPosible;
+
+    typedef struct _caracterFrecuencia {
+        char caracter;
+        unsigned long frecuencia;
+    } caracterFrecuencia;
+
+    ///atributos
+    static char *bufferCriptograma;
+    static vector<longitudPosible> longPosibles;
+    static const vector<string> palabrasConocidas;
+
+    ///métodos
+
+    /** carga el texto en el buffer, eliminando espacios, signos de
+     * puntuación, etc. si fuera necesario */
+    ///Devuelve false en caso de error
+    static bool LeerCriptograma(const char* rutaCriptograma);
+    /** busca cadenas repetidas en el buffer, calcula la distancia entre
+     * ellas, calcula el mcd entre ese número y los que ya están en el vector
+     * de longitudes posibles de la clave, y agrega cada uno de los valores
+     * obtenidos a dicho vector. */
+    ///Devuelve false en caso de error
+    static bool ProcesarCadenasRepetidas();
+    /** agrega un valor al vector de longitudes posibles, o incrementa
+     * su número de ocurrencias si ya estaba en el vector. */
+    ///Devuelve false en caso de error
+    static bool AgregarLongitud(unsigned long longitud);
+    /** saca del vector de longitudes posibles la que tenga más ocurrencias,
+     * y la devuelve. */
+    ///Devuelve 0 en caso de error
+    static unsigned long ObtenerLongitudMasProbable();
+    /** busca en el texto las palabras del vector de palabras conocidas.
+     * Devuelve cuántas de esas palabras fueron encontradas. */
+    ///Devuelve -1 en caso de error
+    static int BuscarPalabrasConocidas(const char* rutaTexto);
+    /** Libera la memoria reservada por las estructuras de la clase. */
+    static void LiberarMemoria();
+
+public:
+    /** Usando el método de Kasiski y análisis de frecuencias obtiene
+     * varias claves posibles, por cada una genera el archivo supuestamente
+     * desencriptado y busca en el las palabras conocidas. Deja sólo el archivo
+     * en el que más de esas palabras aparezcan. Si no aparece ninguna en ningún
+     * archivo, deja aquel que crea que más probablemente sea el correcto (porque
+     * se lo obtuvo haciendo las sustituciones de símbolos con más probabilidades
+     * de ser las correctas, por ejemplo). Los archivos generados se guardan en
+     * rutaDestino. Antes de terminar llama a LiberarMemoria. */
+    ///Devuelve false en caso de error.
+    static bool Romper(const char* rutaCriptograma, const char* rutaDestino);
+};
+
+#endif	/* KASISKI_H */
+
